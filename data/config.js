26aa888b13e1a4861ccebe8c35b1c5715b372cbb
mamarasskazhi <mama.rasskazhi.gl@gmail.com>
2026-08-22 08:15:40 +0500

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,3 @@
+// Если позже опубликуете Google Sheets как CSV, вставьте ссылку между кавычками.
+// Пока поле пустое, сайт использует библиотеку из data/stories.js.
+window.MR_CONFIG = { googleSheetCsvUrl: '' };
